dd1fa2622be4c591bcdaa993aecb9828a7f08cb1
PietruszkaM <pietruszka@agh.edu.pl>
2017-12-03 22:08:18 +0100

    final results obtained in the file results_finished

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ if __name__ == "__main__":
     delim = ";"
 
     with open(file_path, 'w+') as f:
-        for i in range(217,771):#164):
+        for i in range(332,771):#164):
             print(i)
             URL = 'https://prog.nfz.gov.pl/app-jgp/AnalizaPrzekrojowaSzczegoly.aspx?id='+str(i)
             soup = BeautifulSoup(urlopen(URL))
